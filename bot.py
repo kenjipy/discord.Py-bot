c1f5548c5178b21a1c7f64c4601b25fdf34b4248
kenjipy <80217235+kenjipy@users.noreply.github.com>
2021-03-15 19:14:48 +0100

    Update bot.py
    added server info command

--- bot.py
+++ bot.py
@@ -31,6 +31,31 @@ async def mute(ctx, self, user: discord.Member, *, reason=None):
     await member.mute(reason=reason)
     await ctx.send(f' user {member} has been muted.')
     
+  @client.command()
+async def server(ctx):
+    name = str(ctx.guild.name)
+    description = str(ctx.guild.description)
+
+    owner = str(ctx.guild.owner)
+    id = str(ctx.guild.id)
+    region = str(ctx.guild.region)
+    memberCount = str(ctx.guild.member_count)
+
+    icon = str(ctx.guild.icon_url)
+
+    embed = discord.Embed(
+        title=name + " Server Information",
+        description=description,
+        color=discord.Color.blue()
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed)
+    
  
 @client.command()
 @commands.has_permissions(ban_members=True)
