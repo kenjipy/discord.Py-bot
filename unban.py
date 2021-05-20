27f3a68f280fc5f7c64bec10441ff65bc515a1bf
kenjipy <80217235+kenjipy@users.noreply.github.com>
2021-05-20 14:08:31 +0200

    Create unban.py

---
+++ unban.py
@@ -0,0 +1,13 @@
+@client.command()
+@commands.has_permissions(Kick_member=True)
+async def unban(ctx, *, member):
+    bannedUsers = await ctx.guild.bans()
+    name, discriminator = member.split("#")
+
+    for ban in bannedUsers:
+        user = ban.user
+
+        if(user.name, user.discriminator) == (name, discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f"{user.mention} was unbanned.")
+            return
